2ab575dd7a4971b7d16e6fb4b2c48e542bd10962
Leo Suarez <Leo@DTRaleigh.com>
2018-06-03 21:09:34 -0400

    Added query to get all dev_ids, iterate through them in batches of 1000, then added comments on how I should process them.

--- develop/management/commands/actions.py
+++ develop/management/commands/actions.py
@@ -15,6 +15,12 @@ def get_api_json(url):
     return None
 
 
+def batch(iterable, n=1):
+    l = len(iterable)
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
+
+
 def get_total_developments(url):
     # Example:
     # {
@@ -24,3 +30,29 @@ def get_total_developments(url):
     json_count = get_api_json(url)
 
     return json_count["count"]
+
+
+def get_all_dev_ids(url):
+    # Example:
+    # {
+    #     "objectIdFieldName": "OBJECTID",
+    #     "objectIds": [
+    #         35811,
+    #         35812,
+    #           .....
+    #         42081,
+    #         42082
+    #       ]
+    # }
+
+    json_object_ids = get_api_json(url)
+
+    return json_object_ids["objectIds"]
+
+
+def get_dev_range_json(url):
+    # returns a range of dev json starting at one ID up until another
+
+    json_of_devs = get_api_json(url)
+
+    return json_of_devs["features"]
--- develop/management/commands/scan.py
+++ develop/management/commands/scan.py
@@ -20,4 +20,49 @@ class Command(BaseCommand):
                                  "&outSR=4326&f=json&returnCountOnly=true")
         total_devs = get_total_developments(total_dev_count_query)
 
-        print(total_devs)
+        print("Total Devs: " + str(total_devs))
+
+        # Get all development ids
+        all_dev_ids_query = ("https://services.arcgis.com/v400IkDOw1ad7Yad/arcgis/rest/services/Development_Plans"
+                             "/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&returnIdsOnly=true"
+                             "&outSR=4326&f=json")
+        all_dev_ids = get_all_dev_ids(all_dev_ids_query)
+
+        # print("Dev ids: ")
+        # print(all_dev_ids)
+
+        # Process the ids in batches of 1000
+        for x in batch(all_dev_ids, 1000):
+            start_dev_id = x[0]
+            end_dev_id = x[-1]
+
+            # Example - where=OBJECTID>=35811 AND OBJECTID<=35816
+            dev_range_query = ("https://services.arcgis.com/v400IkDOw1ad7Yad/arcgis/rest/services/Development_Plans"
+                               "/FeatureServer/0/query?"
+                               "where=OBJECTID>=" + str(start_dev_id) + " AND OBJECTID<=" + str(end_dev_id) +
+                               "&outFields=*&returnGeometry=false&returnIdsOnly=false&outSR=4326&f=json")
+
+            batch_of_devs_json = get_dev_range_json(dev_range_query)
+
+            # print(len(batch_of_devs_json))
+
+            # Process each dev
+            for dev in batch_of_devs_json:
+                # Is the dev new? Do we have it already in the DB?
+                # If no, need to add it
+                if not known_dev(dev):
+                    # create new development object, dev_json_to_django_object()
+                    # save()
+                    pass
+                # If yes, we already know about it
+                # Next question. Is there an update?
+                else:
+                    # create new development object, dev_json_to_django_object()
+                    # get existing from DB
+                    # If the new object is the same as the one in the DB, do nothing
+                    if new_dev == existing_dev:
+                        pass
+                    # If the new object does differ from the one in the DB, update it
+                    else:
+                        new_dev.update()
+
