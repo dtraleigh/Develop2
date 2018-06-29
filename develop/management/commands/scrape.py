4eb3f2fe20c513e83383cff9223ac159be17dfc7
Leo Suarez <leo@dtraleigh.com>
2018-06-28 20:43:42 -0400

    saving

--- develop/management/commands/scrape.py
+++ develop/management/commands/scrape.py
@@ -3,6 +3,7 @@
 # \\\
 import logging, requests
 from bs4 import BeautifulSoup
+from fuzzywuzzy import fuzz
 
 from django.core.management.base import BaseCommand
 from django.core.mail import send_mail
@@ -49,6 +50,40 @@ class Command(BaseCommand):
                 contact_url = page_link + row_tds[4].find("a")["href"]
 
                 try:
+                    known_sr_cases = SiteReviewCases.objects.all()
+
+                    # go through all of them. Criteria of a match:
+                    # 1. fuzz.ratio(case_number, sr_case.case_number) > 90
+                    # 2. fuzz.ratio(project_name, sr_case.project_name) > 90
+                    # 3. fuzz.ratio(cac, sr_case.cac) > 90
+                    # 2 of 3 need to be true
+                    for sr_case in known_sr_cases:
+                        case_number_score = fuzz.ratio(case_number, sr_case.case_number) > 90
+                        project_name_score = fuzz.ratio(project_name, sr_case.project_name) > 90
+                        cac_score = fuzz.ratio(cac, sr_case.cac) > 90
+
+                        total_score = 0
+
+                        if case_number_score >= 90:
+                            total_score += 1
+                        if project_name_score >= 90:
+                            total_score += 1
+                        if cac_score >= 90:
+                            total_score += 1
+
+                        # sr_case is indeed the same as the scanned info
+                        if total_score >= 2:
+                            known_sr_case = sr_case
+                            break
+
+                    # if known_sr_case was found, check for differences
+                    # if known_sr_case was not found, then we assume a new one was added
+                    # need to create
+                    if known_sr_case:
+                        # check for difference between known_sr_case and the variables
+                    else:
+                        # create a new instance
+
 
         else:
             # Send email alert saying that we could not reach the development page, did not get 200
