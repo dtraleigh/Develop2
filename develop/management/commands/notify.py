be5994f9f98d3f92b4d6e59ca4cca05ef6f61013
Leo Suarez <leo@dtraleigh.com>
2018-08-17 15:39:53 -0400

    Changed the develop and prod email subject

--- develop/management/commands/notify.py
+++ develop/management/commands/notify.py
@@ -8,6 +8,7 @@ from datetime import timedelta
 from django.utils import timezone
 from django.core.management.base import BaseCommand
 from django.core.mail import send_mail
+from django.conf import settings
 
 from .actions import create_email_message
 from develop.models import *
@@ -35,7 +36,10 @@ class Command(BaseCommand):
             everything_that_changed.append(zon)
 
         if everything_that_changed:
-            subject = "Update on Development Tracker"
+            if settings.DEVELOP_INSTANCE == "Develop":
+                subject = "Update on Development Tracker [Develop]"
+            else:
+                subject = "Update on Development Tracker"
             message = create_email_message(everything_that_changed)
             email_from = "develop@dtraleigh.com"
             all_active_subscribers = Subscriber.objects.filter(send_emails=True)
